aad83eaf3c8f89c65ded171428693f76d7b76561
omercsx <omercagris@gmail.com>
2024-11-19 12:00:06 -0500

    Firebase added

--- Movies-Firebase/AppDelegate.swift
+++ Movies-Firebase/AppDelegate.swift
@@ -5,15 +5,15 @@
 //  Created by Bolaji Abdul on 2024-11-19.
 //
 
+import FirebaseCore
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        FirebaseApp.configure()
         return true
     }
 
@@ -30,7 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
-
